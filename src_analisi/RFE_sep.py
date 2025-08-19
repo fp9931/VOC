d92a944d03e010fb789406590329d76bc84e6964
Francesco Pierotti <fp.pierotti@gmail.com>
2025-08-19 20:50:53 +0200

    Switch feature selection to mRMR and update results
    Replaced RFE with mRMR for feature selection in classification and regression tasks. Updated result file outputs to reflect the use of mRMR, added ROC metrics to results, and commented out MFCC feature exclusion to allow flexibility. Also removed debug print statements and adjusted the list of feature selection techniques.

--- src_analisi/RFE_sep.py
+++ src_analisi/RFE_sep.py
@@ -4,7 +4,7 @@ import pandas as pd
 import itertools
 from collections import Counter
 
-# from mrmr import mrmr_classif
+from mrmr import mrmr_classif
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, StratifiedShuffleSplit, RandomizedSearchCV
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
@@ -38,17 +38,24 @@ def feature_selection(X_df, X_train, y_train, X_test, task, score, technique):
             X_task = X_task_df[features]
             y_task = pd.Series(y_train, name=score)
 
-            selector = RFE(estimator=estimator, step=1, n_features_to_select=5)
-            selected_mask = selector.fit(X_task.values, y_task.values).support_
-            selected_features.extend([features[i] for i in range(len(features)) if selected_mask[i]])
+            feature_to_select = mrmr_classif(X=X_task, y=y_task, K=5)
+            selected_features.extend(feature_to_select)
+
+            # selector = RFE(estimator=estimator, step=1, n_features_to_select=5)
+            # selected_mask = selector.fit(X_task.values, y_task.values).support_
+            # selected_features.extend([features[i] for i in range(len(features)) if selected_mask[i]])
 
     elif technique == '10%':
         X_task_df = pd.DataFrame(X_train, columns=X_df.columns)
         y_task = pd.Series(y_train, name=score)
         n_features = int(len(X_task_df.columns) * 0.1)
-        selector = RFE(estimator=estimator, step=1, n_features_to_select=n_features)
-        selected_mask = selector.fit(X_task_df, y_task).support_
-        selected_features = [X_task_df.columns[i] for i in range(len(X_task_df.columns)) if selected_mask[i]]
+
+        feature_to_select = mrmr_classif(X=X_task, y=y_task, K=n_features)
+        selected_features.extend(feature_to_select)
+        
+        # selector = RFE(estimator=estimator, step=1, n_features_to_select=n_features)
+        # selected_mask = selector.fit(X_task_df, y_task).support_
+        # selected_features = [X_task_df.columns[i] for i in range(len(X_task_df.columns)) if selected_mask[i]]
 
     elif technique == 'Free':
         X_train_df = pd.DataFrame(X_train, columns=X_df.columns)
@@ -144,7 +151,8 @@ def main_classification(df, y, score, task, name_dataset):
             }},
         ]
 
-    features_technique = ['5', '10%', 'Free']
+    # features_technique = ['5', '10%', 'Free']
+    features_technique = ['5', '10%']
 
     # Split the data into training and test sets
     if score != 'PUMNS_BulbarSubscore':
@@ -191,7 +199,7 @@ def main_classification(df, y, score, task, name_dataset):
 
             if score != 'PUMNS_BulbarSubscore':
                 best_f1_validation = -np.inf
-                # best_roc_validation = -np.inf
+                best_roc_validation = -np.inf
                 best_model = None
                 best_params = None
                 best_name = None
@@ -211,12 +219,8 @@ def main_classification(df, y, score, task, name_dataset):
                 model_class = model_info['model'].__class__
                 model_parameters = model_info['parameters']
 
-                print(model_name, model_class, model_parameters)
-
                 for params in itertools.product(*model_parameters.values()):
                     params = dict(zip(model_parameters.keys(), params))
-
-                    print(params)
 
                     if score != 'PUMNS_BulbarSubscore':
                         f1_validation = []
@@ -255,7 +259,7 @@ def main_classification(df, y, score, task, name_dataset):
                     if score != 'PUMNS_BulbarSubscore':      
                         # mean_f1_validation = np.mean(f1_validation)
                         mean_f1_validation = np.median(f1_validation)
-                        # mean_roc_validation = np.median(roc_validation)
+                        mean_roc_validation = np.median(roc_validation)
                     else:
                         # mean_rmse_validation = np.mean(rmse_validation)
                         mean_rmse_validation = np.median(rmse_validation)
@@ -268,7 +272,7 @@ def main_classification(df, y, score, task, name_dataset):
                     if score != 'PUMNS_BulbarSubscore':
                         if mean_f1_validation > best_f1_validation:
                         # if mean_roc_validation > best_roc_validation:
-                            # best_roc_validation = mean_roc_validation
+                            best_roc_validation = mean_roc_validation
                             best_f1_validation = mean_f1_validation
                             best_technique = technique
                             best_model = model_class(**params)
@@ -299,7 +303,9 @@ def main_classification(df, y, score, task, name_dataset):
                 train_predictions = final_model.predict(X_train_selected)
                 test_predictions = final_model.predict(X_test_selected)
                 train_f1 = f1_score(y_train, train_predictions)
+                train_roc = roc_auc_score(y_train, train_predictions)
                 test_f1 = f1_score(y_test, test_predictions)
+                test_roc = roc_auc_score(y_test, test_predictions)
                 tn, fp, fn, tp = confusion_matrix(y_test, test_predictions).ravel()
                 accuracy = accuracy_score(y_test, test_predictions)
                 precision = precision_score(y_test, test_predictions)
@@ -315,7 +321,9 @@ def main_classification(df, y, score, task, name_dataset):
                 results_classification['Technique'].append(best_technique)
                 results_classification['Voted features'].append(best_features_voted)
                 results_classification['F1 validation'].append(best_f1_validation)
+                results_classification['ROC validation'].append(best_roc_validation)
                 results_classification['F1 train'].append(train_f1)
+                results_classification['ROC train'].append(train_roc)
                 results_classification['True values'].append(y_test)
                 results_classification['Predicted values'].append(test_predictions)
                 results_classification['TN'].append(tn)
@@ -324,13 +332,14 @@ def main_classification(df, y, score, task, name_dataset):
                 results_classification['TP'].append(tp)
                 results_classification['Accuracy'].append(accuracy)
                 results_classification['F1-score'].append(test_f1)
+                results_classification['ROC test'].append(test_roc)
                 results_classification['Recall'].append(recall)
                 results_classification['Precision'].append(precision)
                 results_classification['Specificity'].append(specificity)
                 results_classification['Sensitivity'].append(sensitivity)
 
                 results_df = pd.DataFrame(results_classification)
-                results_df.to_excel(os.path.join(results_path, 'results_classification_sep_noMFCCs.xlsx'), index=False)
+                results_df.to_excel(os.path.join(results_path, 'results_classification_sep_mRMR.xlsx'), index=False)
 
             else:
                 train_predictions = final_model.predict(X_train_selected)
@@ -366,7 +375,7 @@ def main_classification(df, y, score, task, name_dataset):
                 results_regression['R2 test rounded'].append(test_r2_round)
 
                 results_df = pd.DataFrame(results_regression)
-                results_df.to_excel(os.path.join(results_path, 'results_regression_sep_noMFCCs.xlsx'), index=False)
+                results_df.to_excel(os.path.join(results_path, 'results_regression_sep_mRMR.xlsx'), index=False)
 
 
 # Main
@@ -384,18 +393,18 @@ if __name__ == "__main__":
     columns_to_drop_complete = ['subjid', 'category', 'sex', 'ALSFRS-R_SpeechSubscore', 'ALSFRS-R_SwallowingSubscore', 'PUMNS_BulbarSubscore', 'SML11_t', 'SML12_t', 'SML13_t', 'SML21_t', 'SML22_t', 'SML23_t', 'SML31_t', 'SML32_t', 'SML33_t', 'SML41_t', 'SML42_t', 'SML43_t', 'x2D_DCT1_t', 'x2D_DCT2_t', 'x2D_DCT3_t', 'x2D_DCT4_t', 'x2D_DCT5_t', 'x2D_DCT6_t', 'x2D_DCT7_t', 'x2D_DCT8_t', 'x2D_DCT9_t',
                         'SML11_k', 'SML12_k', 'SML13_k', 'SML21_k', 'SML22_k', 'SML23_k', 'SML31_k', 'SML32_k', 'SML33_k', 'SML41_k', 'SML42_k', 'SML43_k', 'x2D_DCT1_k', 'x2D_DCT2_k', 'x2D_DCT3_k', 'x2D_DCT4_k', 'x2D_DCT5_k', 'x2D_DCT6_k', 'x2D_DCT7_k', 'x2D_DCT8_k', 'x2D_DCT9_k',
                         'SML11_p', 'SML12_p', 'SML13_p', 'SML21_p', 'SML22_p', 'SML23_p', 'SML31_p', 'SML32_p', 'SML33_p', 'SML41_p', 'SML42_p', 'SML43_p', 'x2D_DCT1_p', 'x2D_DCT2_p', 'x2D_DCT3_p', 'x2D_DCT4_p', 'x2D_DCT5_p', 'x2D_DCT6_p', 'x2D_DCT7_p', 'x2D_DCT8_p', 'x2D_DCT9_p', 
-                        'mfcc_0_t', 'mfcc_1_t', 'mfcc_2_t', 'mfcc_3_t', 'mfcc_4_t', 'mfcc_5_t', 'mfcc_6_t', 'mfcc_7_t', 'mfcc_8_t', 'mfcc_9_t', 'mfcc_10_t', 'mfcc_11_t',
-                        'mfcc_0_k', 'mfcc_1_k', 'mfcc_2_k', 'mfcc_3_k', 'mfcc_4_k', 'mfcc_5_k', 'mfcc_6_k', 'mfcc_7_k', 'mfcc_8_k', 'mfcc_9_k', 'mfcc_10_k', 'mfcc_11_k',
-                        'mfcc_0_p', 'mfcc_1_p', 'mfcc_2_p', 'mfcc_3_p', 'mfcc_4_p', 'mfcc_5_p', 'mfcc_6_p', 'mfcc_7_p', 'mfcc_8_p', 'mfcc_9_p', 'mfcc_10_p', 'mfcc_11_p'
+                        # 'mfcc_0_t', 'mfcc_1_t', 'mfcc_2_t', 'mfcc_3_t', 'mfcc_4_t', 'mfcc_5_t', 'mfcc_6_t', 'mfcc_7_t', 'mfcc_8_t', 'mfcc_9_t', 'mfcc_10_t', 'mfcc_11_t',
+                        # 'mfcc_0_k', 'mfcc_1_k', 'mfcc_2_k', 'mfcc_3_k', 'mfcc_4_k', 'mfcc_5_k', 'mfcc_6_k', 'mfcc_7_k', 'mfcc_8_k', 'mfcc_9_k', 'mfcc_10_k', 'mfcc_11_k',
+                        # 'mfcc_0_p', 'mfcc_1_p', 'mfcc_2_p', 'mfcc_3_p', 'mfcc_4_p', 'mfcc_5_p', 'mfcc_6_p', 'mfcc_7_p', 'mfcc_8_p', 'mfcc_9_p', 'mfcc_10_p', 'mfcc_11_p'
                         ]
     
     columns_to_drop_syllable = columns_to_drop_complete
     columns_to_drop_vowels = ['subjid', 'category', 'sex', 'ALSFRS-R_SpeechSubscore', 'ALSFRS-R_SwallowingSubscore', 'PUMNS_BulbarSubscore',
-                              'mfcc_0_a', 'mfcc_1_a', 'mfcc_2_a', 'mfcc_3_a', 'mfcc_4_a', 'mfcc_5_a', 'mfcc_6_a', 'mfcc_7_a', 'mfcc_8_a', 'mfcc_9_a', 'mfcc_10_a', 'mfcc_11_a', 'mfcc_12_a',
-                              'mfcc_0_e', 'mfcc_1_e', 'mfcc_2_e', 'mfcc_3_e', 'mfcc_4_e', 'mfcc_5_e', 'mfcc_6_e', 'mfcc_7_e', 'mfcc_8_e', 'mfcc_9_e', 'mfcc_10_e', 'mfcc_11_e', 'mfcc_12_e',
-                              'mfcc_0_i', 'mfcc_1_i', 'mfcc_2_i', 'mfcc_3_i', 'mfcc_4_i', 'mfcc_5_i', 'mfcc_6_i', 'mfcc_7_i', 'mfcc_8_i', 'mfcc_9_i', 'mfcc_10_i', 'mfcc_11_i', 'mfcc_12_i',
-                              'mfcc_0_o', 'mfcc_1_o', 'mfcc_2_o', 'mfcc_3_o', 'mfcc_4_o', 'mfcc_5_o', 'mfcc_6_o', 'mfcc_7_o', 'mfcc_8_o', 'mfcc_9_o', 'mfcc_10_o', 'mfcc_11_o', 'mfcc_12_o',
-                              'mfcc_0_u', 'mfcc_1_u', 'mfcc_2_u', 'mfcc_3_u', 'mfcc_4_u', 'mfcc_5_u', 'mfcc_6_u', 'mfcc_7_u', 'mfcc_8_u', 'mfcc_9_u', 'mfcc_10_u', 'mfcc_11_u', 'mfcc_12_u'
+                            #   'mfcc_0_a', 'mfcc_1_a', 'mfcc_2_a', 'mfcc_3_a', 'mfcc_4_a', 'mfcc_5_a', 'mfcc_6_a', 'mfcc_7_a', 'mfcc_8_a', 'mfcc_9_a', 'mfcc_10_a', 'mfcc_11_a', 'mfcc_12_a',
+                            #   'mfcc_0_e', 'mfcc_1_e', 'mfcc_2_e', 'mfcc_3_e', 'mfcc_4_e', 'mfcc_5_e', 'mfcc_6_e', 'mfcc_7_e', 'mfcc_8_e', 'mfcc_9_e', 'mfcc_10_e', 'mfcc_11_e', 'mfcc_12_e',
+                            #   'mfcc_0_i', 'mfcc_1_i', 'mfcc_2_i', 'mfcc_3_i', 'mfcc_4_i', 'mfcc_5_i', 'mfcc_6_i', 'mfcc_7_i', 'mfcc_8_i', 'mfcc_9_i', 'mfcc_10_i', 'mfcc_11_i', 'mfcc_12_i',
+                            #   'mfcc_0_o', 'mfcc_1_o', 'mfcc_2_o', 'mfcc_3_o', 'mfcc_4_o', 'mfcc_5_o', 'mfcc_6_o', 'mfcc_7_o', 'mfcc_8_o', 'mfcc_9_o', 'mfcc_10_o', 'mfcc_11_o', 'mfcc_12_o',
+                            #   'mfcc_0_u', 'mfcc_1_u', 'mfcc_2_u', 'mfcc_3_u', 'mfcc_4_u', 'mfcc_5_u', 'mfcc_6_u', 'mfcc_7_u', 'mfcc_8_u', 'mfcc_9_u', 'mfcc_10_u', 'mfcc_11_u', 'mfcc_12_u'
                               ]
 
     task_complete = ['_a','_e', '_i', '_o', '_u', '_k', '_p', '_t']
@@ -428,7 +437,9 @@ if __name__ == "__main__":
         'Technique': [],
         'Voted features': [],
         'F1 validation': [],
+        'ROC validation': [],
         'F1 train': [],
+        'ROC train': [],
         'True values': [],
         'Predicted values': [],
         'TP': [],
@@ -437,6 +448,7 @@ if __name__ == "__main__":
         'FN': [],
         'Accuracy': [],
         'F1-score': [],
+        'ROC test': [],
         'Recall': [],
         'Precision': [],
         'Specificity': [],
